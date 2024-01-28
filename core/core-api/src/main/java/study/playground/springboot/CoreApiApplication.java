3fc2a173ef1acba427f43cfb6181703ae0a68cfa
JanghoPark <pjhsk113@naver.com>
2024-01-28 15:37:02 +0900

    refactor: main 클래스명 변경 및 실행 가능한 모듈로 변경

---
+++ core/core-api/src/main/java/study/playground/springboot/CoreApiApplication.java
@@ -0,0 +1,13 @@
+package study.playground.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+
+@ConfigurationPropertiesScan
+@SpringBootApplication
+public class CoreApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CoreApiApplication.class, args);
+    }
+}
--- core/core-api/src/main/java/study/playground/springboot/Main.java
+++
@@ -1,7 +0,0 @@
-package study.playground.springboot;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
