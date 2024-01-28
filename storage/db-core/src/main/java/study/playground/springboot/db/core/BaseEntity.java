b8804ec1fb5c9bbb8c780449a4d096a1c716162b
JanghoPark <pjhsk113@naver.com>
2024-01-28 15:35:26 +0900

    feat: baseEntity 추가

---
+++ storage/db-core/src/main/java/study/playground/springboot/db/core/BaseEntity.java
@@ -0,0 +1,36 @@
+package study.playground.springboot.db.core;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreationTimestamp
+    @Column
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column
+    private LocalDateTime updatedAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+}
